bec1bd382f63c7d570e704179d0a6474303f8fef
Tom Santos <tommysantos14@gmail.com>
2025-01-22 21:32:46 -0800

    Added .env

---
+++ src/app/actions.ts
@@ -0,0 +1,9 @@
+"use server"
+import { neon } from "@neondatabase/serverless"
+import { env } from "node:process"
+
+export async function getData() {
+	//const sql = neon(process.env.DATABASE_URL)
+	//const data = await sql`...`
+	//return data
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
+          <li className="mb-2 text-blue-500">
+            Get started by editing FOOO544654654654654OOOOOOOO...{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
