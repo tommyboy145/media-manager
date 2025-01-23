fee6b09f90859b40c54096a1239571bdd1120b74
Tom Santos <tommysantos14@gmail.com>
2025-01-22 21:38:56 -0800

    env

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,7 +3,7 @@ import { neon } from "@neondatabase/serverless"
 import { env } from "node:process"
 
 export async function getData() {
-	//const sql = neon(process.env.DATABASE_URL)
+	const sql = neon(process.env.DATABASE_URL)
 	//const data = await sql`...`
 	//return data
 }
